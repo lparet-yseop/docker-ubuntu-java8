4572f06316080f0b470b8fa178dbb14d7a66a625
lparet <lparet@yseop.com>
2017-12-05 11:53:22 +0100

    Add option --no-cache : build without cache

--- ubuntu-java8/build.sh
+++ ubuntu-java8/build.sh
@@ -3,5 +3,5 @@ set -e
 
 source version.sh
 
-echo "Execute : docker build -t ${REGISTRY}/${IMAGE} . "
+echo "Execute : docker build --no-cache -t ${REGISTRY}/${IMAGE} . "
 docker build -t ${REGISTRY}/${IMAGE} .
